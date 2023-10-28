039cdfcaddbb1a652c46a6c0e9ce4121e4054ab4
DanielMesaGarcia <daniwlmesa12@gmail.com>
2023-10-28 14:01:55 +0100

    Falta formatear y ya está hecho

--- front/src/app/detalles/detalles.page.ts
+++ front/src/app/detalles/detalles.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DetallesService } from '../services/detalle.service';
 
 @Component({
   selector: 'app-detalles',
@@ -6,10 +7,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalles.page.scss'],
 })
 export class DetallesPage implements OnInit {
+  detalles: any = [];
+  cantidad: string = '';
+  producto: string = '';
+  idpedido: string = '';
 
-  constructor() { }
+  constructor(private detallesService: DetallesService) {}
 
   ngOnInit() {
+    this.getAllDetalles();
   }
 
+  getAllDetalles() {
+    this.detallesService.getDetalles().subscribe(
+      (detallesResponse) => {
+        this.detalles = detallesResponse;
+        this.detallesService.getPedidos().subscribe(
+          (pedidosResponse) => {
+            this.detalles.forEach((detalle: any) => {
+              detalle.pedido = pedidosResponse.find((pedido: any) => pedido.id === detalle.idpedido);
+            });
+          },
+          (error) => {
+            console.error('Error fetching orders: ', error);
+          }
+        );
+      },
+      (error) => {
+        console.error('Error fetching details: ', error);
+      }
+    );
+  }
+
+  crearDetalle() {
+    const nuevoDetalle = {
+      cantidad: this.cantidad,
+      producto: this.producto,
+    };
+    this.detallesService.createDetalle(this.cantidad, this.producto, this.idpedido).subscribe(
+      (response) => {
+        this.getAllDetalles();
+        this.resetForm();
+      },
+      (error) => {
+        console.error('Error creating detail: ', error);
+      }
+    );
+  }
+
+  updateDetalle(id: number) {
+    this.detallesService.updateDetalle(id, this.cantidad, this.producto, this.idpedido).subscribe(() => {
+      this.getAllDetalles();
+    });
+  }
+
+  deleteDetalle(id: number) {
+    this.detallesService.deleteDetalle(id).subscribe(
+      (response) => {
+        this.getAllDetalles();
+      },
+      (error) => {
+        console.error('Error deleting detail: ', error);
+      }
+    );
+  }
+
+  resetForm() {
+    this.cantidad = '';
+    this.producto = '';
+  }
 }
